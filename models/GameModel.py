162bf1abebfbd219c5b096e140866e576e1651da
Mathieu <mathieu.busse@live.fr>
2024-03-25 13:56:43 +0100

    add name game

--- models/GameModel.py
+++ models/GameModel.py
@@ -2,9 +2,10 @@ from models.db import DbModel
 
 
 class GameModel(DbModel):
-    def __init__(self, game_id: str):
+    def __init__(self, game_id: str, name: str):
         super().__init__()
         self.id: str = game_id
+        self.name: str = name
 
     def __str__(self):
         return f"GameModel('id': '{self.id}')"
