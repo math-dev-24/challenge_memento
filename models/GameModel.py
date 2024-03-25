4380c0fb77b998507da4dc3a80aa115f902ed962
Mathieu <mathieu.busse@live.fr>
2024-03-25 14:45:19 +0100

    add level in gameModel

--- models/GameModel.py
+++ models/GameModel.py
@@ -2,10 +2,11 @@ from models.db import DbModel
 
 
 class GameModel(DbModel):
-    def __init__(self, game_id: str, name: str):
+    def __init__(self, game_id: str, name: str, level: int):
         super().__init__()
         self.id: str = game_id
         self.name: str = name
+        self.level: int = level
 
     def __str__(self):
         return f"GameModel('id': '{self.id}')"
