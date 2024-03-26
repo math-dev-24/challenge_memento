11a3833dc39f332ba12d6e4a73a3d90981db01f8
Mathieu <mathieu.busse@live.fr>
2024-03-26 10:51:31 +0100

    add class Printer

---
+++ core/Print.py
@@ -0,0 +1,28 @@
+from term_printer import Color, Format, cprint
+
+
+class Message:
+
+    @staticmethod
+    def title(text: str):
+        cprint(f" {text} ", attrs=[Format.BOLD, Color.BG_MAGENTA])
+
+    @staticmethod
+    def warning(text: str):
+        cprint(f'🚨 - {text}', attrs=[Color.BRIGHT_RED])
+
+    @staticmethod
+    def ok(text: str):
+        cprint(f'✅️ - {text} ', attrs=[Color.BRIGHT_GREEN])
+
+    @staticmethod
+    def info(text: str):
+        cprint(f'ℹ️ - {text} ', attrs=[Color.BRIGHT_BLUE])
+
+    @staticmethod
+    def question(text: str):
+        cprint(f'{text} ', attrs=[Color.MAGENTA])
+
+    @staticmethod
+    def msg(text: str):
+        print(text)
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from term_printer import Color, Format, cprint
+from core.Print import Message
 from models.GameModel import GameModel
 from models.blockModel import BlockModel
 from models.db import DbModel
@@ -13,26 +14,14 @@ class Game:
         self.db = DbModel()
         self.name = ""
 
-    @staticmethod
-    def msg_warning(text: str):
-        cprint(f'🚨 - {text}', attrs=[Color.BRIGHT_RED])
-
-    @staticmethod
-    def msg_ok(text: str):
-        cprint(f'✅️ - {text} ', attrs=[Color.BRIGHT_GREEN])
-
-    @staticmethod
-    def msg_info(text: str):
-        cprint(f'ℹ️ - {text} ', attrs=[Color.BRIGHT_BLUE])
-
     def init(self):
-        cprint(" Jeu de pair ", attrs=[Format.BOLD, Color.BG_MAGENTA])
+        Message.title(" Jeu de pair ")
         is_n_ok = True
         while is_n_ok:
             print("\n")
-            self.msg_info("Que souhaitez vous faire ?")
-            cprint("1. Reprendre une partie sauvegardé", attrs=[Format.BOLD, Color.CYAN])
-            cprint("2. Nouvelle partie", attrs=[Format.BOLD, Color.MAGENTA])
+            Message.question("Que souhaitez vous faire ?")
+            Message.msg("1. Reprendre une partie sauvegardé")
+            Message.msg("2. Nouvelle partie")
             value = input("> ")
             try:
                 number = int(value)
@@ -43,15 +32,15 @@ class Game:
                         if len(game) > 0:
                             print(self.db.get_all_game())
                         else:
-                            self.msg_info("Aucun jeu disponible")
+                            Message.info("Aucun jeu disponible")
                             is_n_ok = True
                     elif number == 2:
-                        self.name = input('Nom de la partie  > ')
+                        self.name = input('Nom de la partie  > ').strip()
                         self.init_game()
                     else:
-                        self.msg_info(f"Numéros {number} n'est pas 1 ou 2")
+                        Message.warning(f"Numéros {number} n'est pas 1 ou 2")
             except:
-                self.msg_warning("Doit être un nombre !")
+                Message.warning("Doit être un nombre !")
 
     def init_game(self):
         choice_level: bool = False
@@ -59,16 +48,16 @@ class Game:
         while not choice_level:
             cprint('Niveaux disponibles :', attrs=[Format.UNDERLINE, Format.BOLD, Color.MAGENTA])
             for index, grid in enumerate(self.config['level']):
-                cprint(f"Niveau {index + 1} : {grid} x {grid}", attrs=[Color.BRIGHT_GREEN])
+                Message.msg(f"Niveau {index + 1} : {grid} x {grid}")
             choice = input(f"> ")
             try:
                 self.level = int(choice)
                 if q_level >= self.level > 0:
                     choice_level = True
-                    self.msg_ok(f"Partie : nom -> {self.name} et niveau {self.level}")
+                    Message.ok(f"Partie : nom -> {self.name} et niveau {self.level}")
                 else:
                     print('\n' * 20)
-                    self.msg_info(f"Le niveau doit être compris entre 1 et {q_level}")
+                    Message.warning(f"Le niveau doit être compris entre 1 et {q_level}")
             except:
                 print('\n' * 20)
-                self.msg_warning('le niveau doit être un nombre !')
+                Message.warning('le niveau doit être un nombre !')
