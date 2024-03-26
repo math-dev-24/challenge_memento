e7ddd18b18aea269bc574491cea45ebf96f43e9e
Mathieu <mathieu.busse@live.fr>
2024-03-26 10:54:26 +0100

    comment

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import yaml
 from game import Game
-from models.db import DbModel
 
-with open("config.yaml", "r", encoding='utf-8') as f:
-    CONFIG = yaml.load(f, Loader=yaml.FullLoader)
+# Récupération de la configuration
+with open("config.yaml", "r", encoding='utf-8') as config:
+    CONFIG = yaml.load(config, Loader=yaml.FullLoader)
 
+# Instance du jeu
 game = Game(CONFIG)
 
-
+# Initialisation du jeu
 game.init()
 
 
