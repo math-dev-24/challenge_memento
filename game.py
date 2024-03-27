f3a00fc5299b04905f139c6b2f4cc6b6977c1e05
Mathieu_24 <mathieu.busse@live.fr>
2024-03-27 11:16:52 +0100

    mise en place logique génération de la grille et affichage

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from models.GameModel import GameModel
 from models.blockModel import BlockModel
 from models.db import DbModel
 import uuid
+import string
 import random
 
 
@@ -16,6 +17,7 @@ class Game:
         self.blocks = []
         self.gameModel = ""
         self.grid_size: int = 0
+        self.y_list = []
 
     def init(self):
         Message.title(" Jeu de pair ")
@@ -62,7 +64,11 @@ class Game:
                 if q_level >= level > 0:
                     choice_level = True
                     self.grid_size = self.config['level'][level - 1]
-                    Message.ok(f"Partie : nom -> {self.name} et niveau {level} : {self.grid_size} x {self.grid_size}")
+                    self.y_list = self.generate_alphabet(self.grid_size)
+                    print('\n' * 20)
+                    Message.info(f"Partie initialisé !")
+                    Message.info(f"Nom : {self.name}")
+                    Message.info(f"niveau {level} : {self.grid_size} x {self.grid_size}")
                     self.init_grid()
 
                 else:
@@ -75,17 +81,47 @@ class Game:
     def init_grid(self):
         q_emoji = int((self.grid_size * self.grid_size) / 2)
         random.shuffle(self.config['recto'])
-        emoji = self.config['recto'][:q_emoji]
+        emojis = self.config['recto'][:q_emoji]
+        list_emojis = [{'available': 2, 'content': emoji} for emoji in emojis]
 
         for x in range(self.grid_size):
             for y in range(self.grid_size):
-                block = BlockModel('X', False, self.game_id, x + 1, y + 1)
+                list_emojis = [emoji_item for emoji_item in list_emojis if emoji_item['available'] > 0]
+                emoji_number = int(round(random.random() * (len(list_emojis) - 1), 0))
+                current_emoji = list_emojis[emoji_number]['content']
+                list_emojis[emoji_number]['available'] -= 1
+                block = BlockModel(current_emoji, False, self.game_id, x + 1, self.y_list[y])
                 self.blocks.append(block)
-        print(q_emoji)
-        print(emoji)
+        self.print_grid()
 
     def print_grid(self):
-        print(f"{self.grid_size} x {self.grid_size}")
+        print(f"   {' | '.join(self.y_list)}")
+        for x in range(self.grid_size):
+            block_line = [block for block in self.blocks if block.x == (x+1)]
+            line = []
+            for block in block_line:
+                content = block.content if block.is_visible else self.config['verso']
+                line.append(content)
+            print(f'{x + 1} {"| ".join(line)}')
+        self.question_coor()
+
+    def question_coor(self):
+        is_n_ok = True
+        while is_n_ok:
+            print('\n')
+            Message.question("Donnez deux positions !")
+            Message.info("Exemple : A2,B5")
+            coordinate = Message.input().split(",")
+            is_n_ok = not len(coordinate) == 2
+            if is_n_ok:
+                Message.warning("Positions invalides !")
+            else:
+                Message.msg("vérification des coordonées")
+
+    @staticmethod
+    def generate_alphabet(n: int):
+        alphabet = string.ascii_uppercase
+        return [alphabet[i % 26] for i in range(n)]
 
     def get_game_model(self):
         return GameModel(self.game_id, self.name, self.grid_size)
